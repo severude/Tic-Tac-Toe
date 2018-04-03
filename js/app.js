21b5f5ec242db4c361f3c2b5ac8b1ca5ad8f2972
Dale Severude <dseverude@msn.com>
2018-04-03 15:20:23 -0700

    Added final results code

--- js/app.js
+++ js/app.js
@@ -5,29 +5,23 @@
 
 	// Element selectors
 	const start = document.getElementById('start');
-	const button = document.querySelector('.button');
+	const buttons = document.querySelectorAll('.button');
 	const board = document.getElementById('board');
 	const player1 = document.getElementById('player1');
 	const player2 = document.getElementById('player2');
 	const boxes = document.querySelector('.boxes');
 	const finish = document.getElementById('finish');
+	const message = document.querySelector('.message');
 
-	// Turn off board and finish screens before starting the game
+	// Turn off board and finish screens
 	board.style.display = 'none';
 	finish.style.display = 'none';
 	let player = 1; // Player 1 starts with "O"
+	let boxList = []; // Array for box objects
 
-	// Click to start the game and turn on the board
-	button.onclick = () => {
-		start.style.display = 'none';
-		board.style.display = 'block';
-		player1.className = 'players active';
-	};
-	
-	// Give each box a unique identifier
+	// Give each box element a unique integer id
 	for(let index = 0; index < boxes.children.length; index++) {
-		let li = boxes.children[index];
-		li.id = index;
+		boxes.children[index].id = index;
 	}
 
 	// Constructor for each box
@@ -36,48 +30,100 @@
 		this.mark = "";
 	}
 
-	// Array of Box objects
-	let boxList = [new Box(1), new Box(2), new Box(3),
+	// Reset everything to start the game
+	function startGame() {
+		// Show the game board with player 1 selected
+		start.style.display = 'none';
+		board.style.display = 'block';
+		finish.style.display = 'none';
+		player1.className = 'players active';
+		player2.className = 'players';
+		player = 1;
+
+		// Clear all element classes from previous game
+		for(let index = 0; index < boxes.children.length; index++) {
+			boxes.children[index].className = 'box';
+		}
+		
+		// Reset all boxes as unmarked
+		boxList = [new Box(1), new Box(2), new Box(3),
 				   new Box(4), new Box(5), new Box(6),
 				   new Box(7), new Box(8), new Box(9)];
+	}
 
+	// Test to see if game has ended
+	function gameIsOver() {
+		let gameOver = 1;
+		// If every box is marked, then the game is over
+		for(let index = 0; index < boxList.length; index++) {
+			if(boxList[index].mark === "") {
+				gameOver = 0;
+			}
+		}
+		return gameOver;
+	}
+	
+	// Show final game results
+	function showResults() {
+		board.style.display = 'none';
+		finish.style.display = 'block';
+		if (player === 0) {
+			finish.className = 'screen screen-win screen-win-tie';
+			message.textContent = "It's a Tie!";
+		} else if (player === 1) {
+			finish.className = 'screen screen-win screen-win-one';
+			message.textContent = "Winner";
+		} else if (player === 2) {
+			finish.className = 'screen screen-win screen-win-two';
+			message.textContent = "Winner";
+		}
+	}
+	
+	// Buttons to start the game
+	buttons[0].addEventListener('click', startGame);
+	buttons[1].addEventListener('click', startGame);
+	
 	// Show X or O when hovering over an empty box
 	boxes.addEventListener('mouseover', (event) => {
-		let selectedElement = event.target;
 		// Only change if box is unmarked
-		if(boxList[selectedElement.id].mark === "") {
+		if(boxList[event.target.id].mark === "") {
+			// Show which player can check the box
 			if (player === 1) {
-				selectedElement.style.backgroundImage = 'url(../img/o.svg)';
+				event.target.style.backgroundImage = 'url(../img/o.svg)';
 			} else if (player === 2) {
-				selectedElement.style.backgroundImage = 'url(../img/x.svg)';
+				event.target.style.backgroundImage = 'url(../img/x.svg)';
 			}
 		}
 	});
 
 
-	// Remove X or O when hovering over an empty box
+	// Remove X or O when leaving an empty box
 	boxes.addEventListener('mouseout', (event) => {
 		event.target.style.backgroundImage = "";
 	});
 
 	// Mark an empty box with an X or O when clicking on it
 	boxes.addEventListener('click', (event) => {
-		let clickedElement = event.target;
-		// Only change if box is unmarked
-		if(boxList[clickedElement.id].mark === "") {
+		// Only allow adding a mark if the box is unmarked
+		if(boxList[event.target.id].mark === "") {
+			// Determine which player is marking a box
 			if (player === 1) {
-				boxList[clickedElement.id].mark = "O";
-				clickedElement.className = 'box box-filled-1';
+				boxList[event.target.id].mark = "O";
+				event.target.className = 'box box-filled-1';
 				player1.className = 'players';
 				player2.className = 'players active';
 				player = 2;
 			} else if (player === 2) {
-				boxList[clickedElement.id].mark = "X";
-				clickedElement.className = 'box box-filled-2';
+				boxList[event.target.id].mark = "X";
+				event.target.className = 'box box-filled-2';
 				player1.className = 'players active';
 				player2.className = 'players';
 				player = 1;
 			}
+			// If the game has ended, then show the results
+			if(gameIsOver()) {
+				showResults();
+			};
 		}
 	});
 
