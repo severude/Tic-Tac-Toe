4b38d2ef87839eb97fba3ccf2073e2ca1e1594d1
Dale Severude <dseverude@msn.com>
2018-04-04 11:50:20 -0700

    Added logic to determine game winner

--- js/app.js
+++ js/app.js
@@ -16,8 +16,9 @@
 	// Turn off board and finish screens
 	board.style.display = 'none';
 	finish.style.display = 'none';
-	let player = 1; // Player 1 starts with "O"
+	let player = 1; // Player 1 starts game with "O"
 	let boxList = []; // Array for box objects
+	let winningPlayer = 0; // Zero means a tie game
 
 	// Give each box element a unique integer id
 	for(let index = 0; index < boxes.children.length; index++) {
@@ -30,15 +31,27 @@
 		this.mark = "";
 	}
 
+	// Board setting for player one's turn
+	function setupPlayerOne() {
+		player1.className = 'players active';
+		player2.className = 'players';
+		player = 1;
+	}
+	
+	// Board setting for player two's turn
+	function setupPlayerTwo() {
+		player1.className = 'players';
+		player2.className = 'players active';
+		player = 2;
+	}
+	
 	// Reset everything to start the game
 	function startGame() {
 		// Show the game board with player 1 selected
 		start.style.display = 'none';
 		board.style.display = 'block';
 		finish.style.display = 'none';
-		player1.className = 'players active';
-		player2.className = 'players';
-		player = 1;
+		setupPlayerOne();
 
 		// Clear all element classes from previous game
 		for(let index = 0; index < boxes.children.length; index++) {
@@ -51,29 +64,54 @@
 				   new Box(7), new Box(8), new Box(9)];
 	}
 
-	// Test to see if game has ended
+	// The game is over if there are three boxes in a row or all squares are full
 	function gameIsOver() {
-		let gameOver = 1;
-		// If every box is marked, then the game is over
+		let gameWon = 0;
+		let squaresCompleted = 1;
+		
+		// If any box is not marked, then the squares are not completed
 		for(let index = 0; index < boxList.length; index++) {
 			if(boxList[index].mark === "") {
-				gameOver = 0;
+				squaresCompleted = 0;
 			}
 		}
-		return gameOver;
+
+		// Test all 8 possible combinations if the game has been won
+		if((boxList[0].mark === boxList[1].mark && boxList[1].mark === boxList[2].mark && boxList[0].mark !== '') ||
+		  (boxList[3].mark === boxList[4].mark && boxList[4].mark === boxList[5].mark && boxList[3].mark !== '') ||
+		  (boxList[6].mark === boxList[7].mark && boxList[7].mark === boxList[8].mark && boxList[6].mark !== '') ||
+		  (boxList[0].mark === boxList[3].mark && boxList[3].mark === boxList[6].mark && boxList[0].mark !== '') ||
+		  (boxList[1].mark === boxList[4].mark && boxList[4].mark === boxList[7].mark && boxList[1].mark !== '') ||
+		  (boxList[2].mark === boxList[5].mark && boxList[5].mark === boxList[8].mark && boxList[2].mark !== '') ||
+		  (boxList[0].mark === boxList[4].mark && boxList[4].mark === boxList[8].mark && boxList[0].mark !== '') ||
+		  (boxList[2].mark === boxList[4].mark && boxList[4].mark === boxList[6].mark && boxList[2].mark !== '')) {
+			gameWon = 1;
+		}
+		
+		// If squares are completed and no one has won, then the game is a tie
+		if(squaresCompleted && !gameWon) {
+			winningPlayer = 0;
+		}
+		
+		// If the game has been won, then determine who won. The last player to play is the winner.
+		if(gameWon) {
+			winningPlayer = (player === 1) ? 2 : 1;
+		}
+		
+		return gameWon || squaresCompleted;
 	}
 	
 	// Show final game results
 	function showResults() {
 		board.style.display = 'none';
 		finish.style.display = 'block';
-		if (player === 0) {
+		if (winningPlayer === 0) {
 			finish.className = 'screen screen-win screen-win-tie';
 			message.textContent = "It's a Tie!";
-		} else if (player === 1) {
+		} else if (winningPlayer === 1) {
 			finish.className = 'screen screen-win screen-win-one';
 			message.textContent = "Winner";
-		} else if (player === 2) {
+		} else if (winningPlayer === 2) {
 			finish.className = 'screen screen-win screen-win-two';
 			message.textContent = "Winner";
 		}
@@ -110,15 +148,11 @@
 			if (player === 1) {
 				boxList[event.target.id].mark = "O";
 				event.target.className = 'box box-filled-1';
-				player1.className = 'players';
-				player2.className = 'players active';
-				player = 2;
+				setupPlayerTwo();
 			} else if (player === 2) {
 				boxList[event.target.id].mark = "X";
 				event.target.className = 'box box-filled-2';
-				player1.className = 'players active';
-				player2.className = 'players';
-				player = 1;
+				setupPlayerOne();
 			}
 			// If the game has ended, then show the results
 			if(gameIsOver()) {
